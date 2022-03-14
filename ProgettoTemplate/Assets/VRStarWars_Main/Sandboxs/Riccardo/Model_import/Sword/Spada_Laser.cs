91e9dbb859cffd4739c8743b4c8cd1a2271832ab
00RikY <rikytrump@gmail.com>
2022-03-14 14:13:45 +0100

    prefab lightseber

--- ProgettoTemplate/Assets/VRStarWars_Main/Sandboxs/Riccardo/Model_import/Sword/Spada_Laser.cs
+++ ProgettoTemplate/Assets/VRStarWars_Main/Sandboxs/Riccardo/Model_import/Sword/Spada_Laser.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Spada_Laser : MonoBehaviour
 {
+    //laser della spada
+    private GameObject laser;
+
+    //dimensione della spada con laser attivo
+    private Vector3 fullSize;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Ricerca del gameobject all'interno della hierarchy
+        laser = transform.Find("LightSaber-Blade").gameObject;
+
+        //trasformiamo la scala locale per salvare qualunque sia la dimensione intera
+        fullSize = laser.transform.localScale;
+
+        //al di là della scala creiamo un nuovo vettore che mette a  0 la y
+        laser.transform.localScale = new Vector3(fullSize.x, 0, fullSize.z);
+
         
     }
 
