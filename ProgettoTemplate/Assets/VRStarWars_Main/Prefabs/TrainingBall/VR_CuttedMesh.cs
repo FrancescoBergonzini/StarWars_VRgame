621479695ec61b757df440979032bda9c0d84d7c
Francesco Bergonzini <fbergonzinigames@gmail.com>
2022-03-28 21:56:43 +0200

    Dsitance grabbing + grabbing mesh tagliate

--- ProgettoTemplate/Assets/VRStarWars_Main/Prefabs/TrainingBall/VR_CuttedMesh.cs
+++ ProgettoTemplate/Assets/VRStarWars_Main/Prefabs/TrainingBall/VR_CuttedMesh.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-namespace SW_VRGame
-{
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(MeshCollider))]
-    public class VR_CuttedMesh : MonoBehaviour
+    namespace SW_VRGame
     {
+        [RequireComponent(typeof(Rigidbody))]
+        [RequireComponent(typeof(MeshCollider))]
+        [RequireComponent(typeof(XRGrabInteractable))]
+        public class VR_CuttedMesh : MonoBehaviour
+        {
+
         public int sliceCounter = 2;
         public float delayCut = 1f;
+            
+        XRGrabInteractable _myGrabController;
+        InteractionLayerMask layerToGrab = InteractionLayerMask.GetMask("Grab");
 
         void Awake()
         {
             this.GetComponent<MeshCollider>().convex = true;
+            _myGrabController = GetComponent<XRGrabInteractable>();
+
+            _myGrabController.throwVelocityScale = 4;
+            _myGrabController.interactionLayers = layerToGrab;
         }
 
         private void Start()
         {
             if (this.gameObject != null)
-            {           
-                Destroy(this.gameObject, 3);
+            {
+                //modifica
+                Destroy(this.gameObject, 5);
             }
 
             StartCoroutine(DelayCut());
@@ -32,5 +43,6 @@ namespace SW_VRGame
             //rendile nuovamente tagliabili dopo un secondo
             this.gameObject.tag = "Sliceable";
         }
+        }
     }
-}
+
